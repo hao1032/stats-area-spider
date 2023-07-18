1f0f6c5a9cb6ccb02a8f18724304ec05bf65f743
tango <hao1032@gmail.com>
2023-07-18 15:02:36 +0800

    增加错误处理，每 5 分钟保存一次

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import re
 import json
+import time
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -13,8 +15,12 @@ class Main(object):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
         }
-        resp = requests.get(url, headers=headers)
-        resp.encoding = 'utf-8'
+        try:
+            resp = requests.get(url, headers=headers, timeout=5)
+            resp.encoding = 'utf-8'
+        except requests.exceptions.RequestException as e:
+            print(f'error: {e}')
+            return '<html></html>'  # 遇到错误返回空，后面会再次处理没有抓取的情况
         return resp.text
 
     def get_children_province(self, bs, url):
@@ -24,13 +30,14 @@ class Main(object):
         :param url:
         :return:
         """
+        base_url = url.replace('index.html', '')
         links = bs.find_all('a', href=re.compile('html'))
         children = []
         for link in links:
             child = {
                 'code': link['href'].replace('.html', '').strip(),
                 'name': link.get_text().strip(),
-                'url': f'{url}/{link["href"]}',
+                'url': f'{base_url}{link["href"]}',
                 'status': "start",  # 标记尚未抓取下级区域
                 'children': []
             }
@@ -44,6 +51,7 @@ class Main(object):
         :param url:
         :return:
         """
+        base_url = url[0: url.rfind('/') + 1]
         table_trs = bs.find_all('tr', class_='citytr')           # 市级
         table_trs.extend(bs.find_all('tr', class_='countytr'))   # 区级
         table_trs.extend(bs.find_all('tr', class_='towntr'))     # 镇级
@@ -52,7 +60,7 @@ class Main(object):
         for tr in table_trs:
             link = tr.find('a')
             table_tds = tr.find_all('td')
-            url = f'{url}/{link["href"]}' if link else ''  # 村级和某些区级没有链接
+            url = f'{base_url}{link["href"]}' if link else ''  # 村级和某些区级没有链接
             child = {
                 'code': table_tds[0].get_text().strip(),
                 'name': table_tds[-1].get_text().strip(),  # 省市区镇有 2 列，村有 3 列，使用 -1 获取名称
@@ -68,7 +76,6 @@ class Main(object):
         # time.sleep(1)  # 防止请求过快
         bs = BeautifulSoup(html, 'lxml')
         is_index = 'index.html' in url  # 判断是否主页，主页需要获取省份和非省份规则不同
-        url = url[0: url.rfind('/')]  # 将本级 URL 去掉最后一级，作为下级的 base url
         children = self.get_children_province(bs, url) if is_index else self.get_children_not_province(bs, url)
         return children
 
@@ -87,7 +94,7 @@ class Main(object):
         :return:
         """
         if isinstance(node, dict):
-            if node.get("status") == "start":
+            if node['status'] == 'start':
                 return node
             for key, value in node.items():
                 result = self.find_start_node(value)
@@ -100,21 +107,29 @@ class Main(object):
                     return result
         return None
 
+    def save_data_file(self):
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, indent=4, ensure_ascii=False)
+
     def run(self):
         # 加载当前已有数据
-        with open('data.json', 'r') as f:
+        with open('data.json', 'rb') as f:
             self.data = json.load(f)
 
         # 通过循环抓取尚未完成的数据
+        start_time = time.time()
         while True:
             node = self.find_start_node(self.data)
             if node:
                 # 抓取一个 node 后，保存数据
                 self.handle_node(node)
-                with open('data.json', 'w') as f:
-                    json.dump(self.data, f, indent=4, ensure_ascii=False)
             else:
                 break
+
+            # 每隔一段时间保存一次，5 分钟
+            if time.time() - start_time > 5 * 60:
+                self.save_data_file()
+                start_time = time.time()
 
 
 Main().run()
